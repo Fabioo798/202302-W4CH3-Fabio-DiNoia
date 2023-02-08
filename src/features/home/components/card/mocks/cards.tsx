01e23383062fc82114fe34c7952351eed306c06b
Fabio <fabiodn.798@gmail.com>
2023-02-08 20:46:45 +0100

    Add mocks cards

---
+++ src/features/home/components/card/mocks/cards.tsx
@@ -0,0 +1,9 @@
+import { CardStructure } from "../model/card";
+
+export const MOCK_CARDS: CardStructure[] = [
+  { name: "Joffrey", family: "Baratheon", age: 12, isLive: true },
+  { name: "Jaime", family: "Lannister", age: 25, isLive: true },
+  { name: "Daenerys", family: "Targaryen", age: 20, isLive: true },
+  { name: "Tyron", family: "Lannister", age: 30, isLive: true },
+  { name: "Bronn", family: "HighTower", age: 35, isLive: true },
+];
