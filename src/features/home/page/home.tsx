fe08083e701bfe9f4fc9a2c418c63538d311e410
Fabio <fabiodn.798@gmail.com>
2023-02-08 20:45:18 +0100

    Add home component + style

---
+++ src/features/home/page/home.tsx
@@ -0,0 +1,11 @@
+import { Card } from "../components/card/card";
+import { Cards } from "../components/cards/cards";
+import "./home.css";
+export function HomePage() {
+  return (
+    <section>
+      <h2>Home Page</h2>
+      <Cards></Cards>
+    </section>
+  );
+}
