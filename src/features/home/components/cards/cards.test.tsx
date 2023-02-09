f0ad01790a3d840428b3d145884cde7c27626ab8
Fabio <fabiodn.798@gmail.com>
2023-02-09 07:52:46 +0100

    Update cards component

--- src/features/home/components/cards/cards.test.tsx
+++ src/features/home/components/cards/cards.test.tsx
@@ -1,14 +1,26 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Card } from "../card/card";
+import { CardStructure } from "../card/model/card";
 
-
-jest.mock("../card/card");
+jest.mock("../card/card", () => ({
+  Card: () => <div data-testid="mock-card">mock card</div>,
+}));
 
 describe("given Cards component", () => {
   describe("when it is render", () => {
-    test("Then it should call card", () => {
-      render(<Card />);
-      expect(Card).toHaveBeenCalled();
+    const mockCard: CardStructure = {
+      name: "Fabio",
+      family: "Snow",
+      age: 24,
+      isLive: true,
+      throneYears: 1,
+      sentence: "vais a morir todos",
+      picture: "fabio.jpg",
+    };
+
+    test("Then it should render the mock card", () => {
+      const { getByTestId } = render(<Card character={mockCard} />);
+      expect(screen.getByTestId("mock-card").textContent).toBe("mock card");
     });
   });
 });
--- src/features/home/components/cards/cards.tsx
+++ src/features/home/components/cards/cards.tsx
@@ -1,9 +1,14 @@
 import { Card } from "../card/card"
+import { MOCK_CARDS } from "../card/mocks/cards"
 
 export function Cards() {
   return (
     <ul className="characters-list row list-unstyled">
-      <Card></Card>
+      {MOCK_CARDS.map((character, index) => (
+        <li key={index} className="col-md-3">
+          <Card character={character} />
+          </li>
+      ))}
     </ul>
   )
 }
