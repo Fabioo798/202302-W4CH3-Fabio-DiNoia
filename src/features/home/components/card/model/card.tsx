c1a921172374b901c102e34952205b1bea0edb5f
Fabio <fabiodn.798@gmail.com>
2023-02-09 07:52:06 +0100

    Update card components

--- src/features/home/components/card/model/card.tsx
+++ src/features/home/components/card/model/card.tsx
@@ -1,6 +1,16 @@
 export type CardStructure = {
+
   name: string;
   family: string;
   age: number;
   isLive: boolean;
+  throneYears?: number;
+  weapon?: string;
+  dexterity?: number;
+  advisor?: string;
+  servant?: string;
+  grade?: number;
+  sentence?: string;
+  picture: string;
+
 }
