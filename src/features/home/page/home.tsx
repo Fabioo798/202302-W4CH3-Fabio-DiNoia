051c7227e712ae3f5182e3ba4d22afab056120ee
Fabio <fabiodn.798@gmail.com>
2023-02-09 07:51:08 +0100

    Update home

--- src/features/home/page/home.tsx
+++ src/features/home/page/home.tsx
@@ -1,10 +1,8 @@
-
 import { Cards } from "../components/cards/cards";
 import "./home.css";
 export function HomePage() {
   return (
     <section>
-      <h2>Home Page</h2>
       <Cards></Cards>
     </section>
   );
